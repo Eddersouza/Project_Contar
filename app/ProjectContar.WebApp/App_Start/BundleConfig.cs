838995478720e3aa29c6abcff3ea7352ffb53a7e
eddersouza <eddye_souza@yahoo.com.br>
2019-04-28 13:43:09 -0300

    Adicionado Materialize

--- app/ProjectContar.WebApp/App_Start/BundleConfig.cs
+++ app/ProjectContar.WebApp/App_Start/BundleConfig.cs
@@ -1,5 +1,4 @@
-﻿using System.Web;
-using System.Web.Optimization;
+﻿using System.Web.Optimization;
 
 namespace ProjectContar.WebApp
 {
@@ -13,6 +12,9 @@ namespace ProjectContar.WebApp
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/materialize").Include(
+                       "~/Scripts/materialize.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
@@ -29,6 +31,9 @@ namespace ProjectContar.WebApp
 
             bundles.Add(new StyleBundle("~/Content/FontAwesome").Include(
                     "~/Content/FontAwesome/all.css"));
+
+            bundles.Add(new StyleBundle("~/Content/materialize").Include(
+                   "~/Content/materialize.css"));
         }
     }
 }
