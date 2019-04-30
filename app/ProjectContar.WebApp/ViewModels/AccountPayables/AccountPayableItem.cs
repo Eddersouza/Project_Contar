46a99632effa173ff69f56e08c1ea0c32e51c611
eddersouza <eddye_souza@yahoo.com.br>
2019-04-29 22:43:25 -0300

    Ajuste vies nova conta a pagar

--- app/ProjectContar.WebApp/ViewModels/AccountPayables/AccountPayableItem.cs
+++ app/ProjectContar.WebApp/ViewModels/AccountPayables/AccountPayableItem.cs
@@ -1,4 +1,6 @@
-﻿namespace ProjectContar.WebApp.ViewModels.AccountPayables
+﻿using System.ComponentModel;
+
+namespace ProjectContar.WebApp.ViewModels.AccountPayables
 {
     /// <summary>
     /// Represents view to item Account Payable.
@@ -12,20 +14,22 @@
         {
         }
 
-
         /// <summary>
         /// Amount account.
         /// </summary>
+        [DisplayName("Valor")]
         public string Amount { get; set; }
 
         /// <summary>
         /// Due Date to Account.
         /// </summary>
+        [DisplayName("Data de Vencimento")]
         public string DueDate { get; set; }
 
         /// <summary>
         /// Name to Account.
         /// </summary>
+        [DisplayName("Nome")]
         public string Name { get; set; }
     }
 }
